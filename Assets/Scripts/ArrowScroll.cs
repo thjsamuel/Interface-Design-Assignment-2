95fe74205e5eed46ce3b35b0f6f907375ad98c52
thjsamuel <153942B@mymail.nyp.edu.sg>
2017-01-25 10:25:50 +0800

    Added language option in Options scene.

---
+++ Assets/Scripts/ArrowScroll.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ArrowScroll : MonoBehaviour {
+    private Text lang_text;
+    private string[] lang_type;
+    public int index;
+    public const int MAX_INDEX = 3;
+	// Use this for initialization
+	void Start () {
+        index = 0;
+        lang_text = transform.GetChild(0).GetComponent<Text>();
+        Debug.Log(lang_text.text);
+        lang_type = new string[MAX_INDEX];
+        lang_type[0] = "English";
+        lang_type[1] = "Mandarin";
+        lang_type[2] = "Japanese";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (index < 0)
+            index = MAX_INDEX;
+        else if (index >= MAX_INDEX)
+            index = 0;
+        if (!lang_text.text.Equals(lang_type[index]))
+            lang_text.text = lang_type[index];
+	}
+
+    public void clickLeft()
+    {
+        --index;
+    }
+
+    public void clickRight()
+    {
+        ++index;
+    }
+}
