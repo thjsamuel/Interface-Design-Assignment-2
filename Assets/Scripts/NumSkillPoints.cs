91612e35bc157023bf2edd6f3628965f71d71c87
raynerlrh <raynerlrh@gmail.com>
2017-01-25 09:36:15 +0800

    Character movement, display number of skill points on character skills page (doesn't receive from character yet, just a set number)

---
+++ Assets/Scripts/NumSkillPoints.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumSkillPoints : MonoBehaviour {
+
+    private Text numSkillPoints;
+
+	// Use this for initialization
+	void Start () {
+        numSkillPoints = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //numSkillPoints = Character.numSkillPointsText;
+        numSkillPoints.text = "hi";
+	}
+}
